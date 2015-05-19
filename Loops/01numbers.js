bf9c4e86417c214b84347598ddf4da9fa2d9f120
dchakov <dchakov@yahoo.com>
2015-05-19 23:16:14 +0300

    js file

---
+++ Loops/01numbers.js
@@ -0,0 +1,9 @@
+﻿//Problem 1. Numbers
+//Write a script that prints all the numbers from  1  to  N .
+
+function funcLoops() {
+    var numberN = document.getElementById('num01').value;
+    for (var i = 1; i <= numberN; i++) {
+        console.log(i);
+    }
+}
