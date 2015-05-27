e2dd965710c9d1b9d8db57175aff28e9d95864bb
dchakov <dchakov@yahoo.com>
2015-05-27 23:33:03 +0300

    string

---
+++ Strings/01reverseString.js
@@ -0,0 +1,12 @@
+﻿//Problem 1. Reverse string
+//Write a JavaScript function that reverses a string and returns it.
+
+function reverseS(string) {
+    return string.split('').reverse().join('');
+}
+
+var string = 'nikolay stoqnov';
+
+function reverseString() {
+    console.log(reverseS(string));
+}
