e2a78928e70b0217a1fe7578ae0a84cef1b4087d
dchakov <dchakov@yahoo.com>
2015-05-29 20:38:57 +0300

    vs commit

--- Strings/06extractTextHTML.js
+++ Strings/06extractTextHTML.js
@@ -3,7 +3,23 @@
 //The function should return anything that is in a tag, without the tags.
 
 function extractText(text) {
-
+    var inTags = false;
+    var plainText = '';
+    for (var i = 0; i < text.length; i++) {
+        if (inTags) {
+            if (text[i] === '>') {
+                inTags = false;
+            } 
+        }
+        else {
+                if (text[i] == '<') {
+                    inTags = true;
+                } else {
+                    plainText += text[i];
+                }
+            }
+    }
+    return plainText;
 }
 
 var textHTML = "<html><head><title>Sample site</title></head><body><div>text<div>more text</div>and more...</div>in body</body></html>";
