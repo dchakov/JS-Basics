8db5a9bbf4583a1d3902b382184ae1b290a71ced
dchakov <dchakov@yahoo.com>
2015-05-29 22:28:52 +0300

    09fin

---
+++ Strings/08replaceTags.js
@@ -0,0 +1,30 @@
+﻿//Problem 8. Replace tags
+//Write a JavaScript function that replaces in a HTML document given as string all the tags  <a href="…">…</a>  with corresponding tags  [URL=…]…/URL] .
+
+
+function replaceHTMLtags(string) {
+    var output = string;
+    var counter = 0;
+    while (output.indexOf('<a href="', counter) > 0) {
+        output = output.replace('<a href="', '[URL=');
+        counter++;
+    }
+    counter = 0;
+    while (output.indexOf('">', counter) > 0) {
+        output = output.replace('">', ']');
+        counter++;
+    }
+    counter = 0;
+    while (output.indexOf('</a>', counter) > 0) {
+        output = output.replace('</a>', '[/URL]');
+        counter++;
+    }
+    return output;
+}
+var htmlDocument = '<p>Please visit <a href="http://academy.telerik.com">our site</a> to choose a training course. Also visit <a href="www.devbg.org">our forum</a> to discuss the courses.</p>';
+
+function replaceTags() {
+    console.log(htmlDocument);
+    console.log(replaceHTMLtags(htmlDocument));
+    
+}
---
+++ Strings/09extractEMails.js
@@ -0,0 +1,14 @@
+﻿//Problem 9. Extract e-mails• Write a function for extracting all email addresses from given text.
+//All sub-strings that match the format @… should be recognized as emails.
+//Return the emails as array of strings.
+
+function extractMail(text,pattern) {
+    return text.match(pattern);
+}
+var textEmails = 'Please assin@any.stup.co.uk contact us baiko_at@hotmail.com  or by email at exampQle@abv.bg or at baj.ivan@yahoo.co.uk. This is not email: test@test. This also: @telerik.com. Neither this: a@a@.b';
+var pattern = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
+
+function extractEMail() {
+    console.log(textEmails);
+    console.log(extractMail(textEmails,pattern).join('\n'));
+}
