4d8d6d626d50c0ca0bf456c5545afaae61c3e67b
dchakov <dchakov@yahoo.com>
2015-06-03 09:16:15 +0300

    final

---
+++ Strings/10findPalindromes.js
@@ -0,0 +1,24 @@
+﻿//Problem 10. Find palindromes
+//Write a program that extracts from a given text all palindromes, e.g. "ABBA", "lamal", "exe".
+
+function searchForPalindromes(string) {
+    var result = [],
+        arrWords,
+        i;
+    arrWords = string.split(/[\s,".]+/);
+    for (i in arrWords) {
+        if (arrWords[i].split('').reverse().join('') === arrWords[i]) {
+            if (arrWords[i].length !== 1 && arrWords[i].length !== 0) {
+                result.push(arrWords[i]);
+            }
+        }
+    }
+    return result;
+}
+//var stringPalindromes = 'Write a program that extracts from a given text all palindromes, e.g. "ABBA", "lamal", "exe".';
+var stringPalindromes = 'Palindromes are very interesting words. Examples for palindromes are ABBA, lamal, exe, rotor. These words can be read the same way in either direction. Other examples -> level, racecar, deed, civic, nun, pop, eye, madam';
+
+function findPalindromes() {
+    console.log(stringPalindromes);
+    console.log(searchForPalindromes(stringPalindromes));
+}
---
+++ Strings/11stringFormat.js
@@ -0,0 +1,22 @@
+﻿//Problem 11. String format
+//Write a function that formats a string using placeholders.
+//The function should work with up to 30 placeholders and all types.
+
+function stringFormat() {
+    var result = arguments[0],
+    counter;
+    for (var i = 0; i < arguments.length - 1; i++) {
+        counter = 0;
+        while (result.indexOf('{' + i + '}', counter) >= 0) {
+            result = result.replace('{' + i + '}', arguments[i + 1]);
+            counter += 1;
+        }
+    }
+    return result;
+}
+var frmt = '{0}, {1}, {0} text {2}';
+
+function formatStringPlace() {
+    console.log(stringFormat('Hello {0}!', 'Peter'));
+    console.log(stringFormat(frmt, 1, 'Pesho', 'Gosho'));
+}
